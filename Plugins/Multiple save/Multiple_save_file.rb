0458d7e7a14a90396fcf25ff4801990c004e9d90
Jonathan Lee <jlee4889@gmail.com>
2025-02-14 10:00:50 +0000

    implement new features
    add toggle to turn weather off
    ensure player's sprite changes when getting on/off bike

--- Plugins/Multiple save/Multiple_save_file.rb
+++ Plugins/Multiple save/Multiple_save_file.rb
@@ -125,12 +125,15 @@ module Game
 		SaveData.changeFILEPATH($storenamefilesave.nil? ? FileSave.name : $storenamefilesave)
     SaveData.move_old_windows_save if System.platform[/Windows/]
     save_data = (SaveData.exists?) ? SaveData.read_from_file(SaveData::FILE_PATH) : {}
+    
     if save_data.empty?
       SaveData.initialize_bootup_values
     else
       SaveData.load_bootup_values(save_data)
+      # Reinitialize settings from the shared system files, if it exists. (Added by: kraegon)
+      $PokemonSystem = load_data(SYSTEM_SETTINGS_FILE) if pbRgssExists?(SYSTEM_SETTINGS_FILE)
     end
-    # Set resize factor
+      # Set resize factor
     pbSetResizeFactor([$PokemonSystem.screensize, 4].min)
     # Set language (and choose language if there is no save file)
     if Settings::LANGUAGES.length >= 2
